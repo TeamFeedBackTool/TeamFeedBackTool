05ebffe7aef729ecf7b0a60688adc52eab5a88cb
Felix Linhart <felix.linhart@htl.rennweg.at>
2018-11-04 15:51:50 +0100

    Bugfixes und Probleme mit MySql Updates

--- ChangeSurname.php
+++ ChangeSurname.php
@@ -1,3 +1,37 @@
 <?php
-//A simple Script that overwrites an entry in the database with the input of the user (Surname)
-session_start();
+    //session_start();
+    require "config.php";
+
+    try {
+        //A simple Script that overwrites an entry in the database with the input of the user (Surname)
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (empty(trim($_POST["surnameChange"]))) {
+                echo("Please enter a surname");
+            } else {
+                echo("Hallo");
+
+                $email = trim($_SESSION['email']);
+                $surname = trim($_POST['surnameChange']);
+                $sql = ("UPDATE users
+                         SET surname = '" . $surname . "'
+                         WHERE email = '" . $email . "'" );
+                echo($sql);
+                // Prepare statement
+                $stmt = $pdo->prepare($sql);
+
+                // execute the query
+                $stmt->execute();
+                echo("Hallo");
+                $row = $stmt->fetch();
+                $_SESSION['surname'] = $row['surname'];
+
+                // echo a message to say the UPDATE succeeded
+                echo $stmt->rowCount() . " records UPDATED successfully";
+                header("location:../HTML - Tests/TestUserProfile.php");
+            }
+        }
+    }
+catch(PDOException $e) {
+    echo $e->getMessage();
+}
+
--- config.php
+++ config.php
@@ -8,6 +8,7 @@ try{
     $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
     // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 } catch(PDOException $e){
     die("ERROR: Could not connect. " . $e->getMessage());
 }
