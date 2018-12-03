7ec31c9f906d28423728c1dae8d5fd8c18b239c8
KevChela <kevin.chelappurath@htl.rennweg.at>
2018-12-03 10:41:25 +0100

    GiveFeedback fertig

--- Frontend/app/components/give-feedback.js
+++ Frontend/app/components/give-feedback.js
@@ -3,9 +3,36 @@ app.component("giveFeedback", {
     controller: "giveFeedbackController"
 });
 
-app.controller("giveFeedbackController", function ($http, $scope){
+app.controller("giveFeedbackController", function ($http, $scope) {
 
     $scope.rating1 = 0;
 
     $scope.rating2 = 0;
+
+    this.submit = () => {
+
+        rating11 = $scope.rating1;
+
+        rating22 = $scope.rating2;
+
+        let parameter = JSON.stringify({
+            stress: rating11,
+            motivation: rating22,
+            satisfied: this.group1,
+            technicalSkills: this.group2
+        });
+
+        let url = "../../Backend/GiveFeedback.php";
+
+        $http({
+            method: 'POST',
+            url: url,
+            data: parameter
+        }).then(
+            (response) => {
+                console.log(response);
+            }, function (error) {
+                console.log(error);
+            });
+    }
 });
