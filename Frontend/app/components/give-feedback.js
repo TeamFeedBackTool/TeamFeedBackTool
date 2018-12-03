5ec227f090ac7c5d05bfaa40760be798d8b505e7
KevChela <kevin.chelappurath@htl.rennweg.at>
2018-12-03 11:15:21 +0100

    Bin ja schon fertig

--- Frontend/app/components/give-feedback.js
+++ Frontend/app/components/give-feedback.js
@@ -16,6 +16,7 @@ app.controller("giveFeedbackController", function ($http, $scope) {
         rating22 = $scope.rating2;
 
         let parameter = JSON.stringify({
+            projectId: 45,
             stress: rating11,
             motivation: rating22,
             satisfied: this.group1,
@@ -30,9 +31,21 @@ app.controller("giveFeedbackController", function ($http, $scope) {
             data: parameter
         }).then(
             (response) => {
+                this.projectId = response.data.projectId;
+                this.info = response.data.infotext;
                 console.log(response);
+                console.log(info)
             }, function (error) {
                 console.log(error);
             });
+
+        if(info === "") {
+
+        }
+
+        document.getElementById("done").style = "display:block; transition:";
+        new Promise((resolve) => setTimeout(resolve, 10000)).then(() => {
+            document.getElementById("done").style = "display: none";
+        });
     }
 });
