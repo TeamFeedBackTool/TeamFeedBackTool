05fca24b50bc90e41f122abab7aef9d66b09254e
Dominik Novi <dominik.novi@htl.rennweg.at>
2018-12-28 15:14:47 +0100

    jokes on me! doesnt work as i want it to

--- Frontend/app/components/employee-dropdown.js
+++ Frontend/app/components/employee-dropdown.js
@@ -3,25 +3,27 @@ app.component("employeeDropdown", {
     controller: "employeeDropdownController"
 });
 
-app.controller("employeeDropdownController", function ($http) {
-    let projId = 1;
+app.controller("employeeDropdownController", function ($http, $rootScope) {
+    let projId = $rootScope.currentProject.id;
     let url = "../../Backend/xxx.php";
 
-    let employees = [];
+    let employees = ["Dominik Novi", "Florian Gödel"];
 
-    let parameter = JSON.stringify({
-        projectId: projId
-    });
 
-    $http({
-        method: 'POST',
-        url: url,
-        data: parameter
-    }).then(
-        (response) => {
-            console.log(response);
-            employees = response.data.employees;
-        }, function (error) {
-            console.log(error);
-        });
+    // let parameter = JSON.stringify({
+    //     projectId: projId
+    // });
+    //
+    // $http({
+    //     method: 'POST',
+    //     url: url,
+    //     data: parameter
+    // }).then(
+    //     (response) => {
+    //         console.log(response);
+    //         employees = response.data.employees;
+    //     }, function (error) {
+    //         console.log(error);
+    //     });
+
 });
