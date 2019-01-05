2a6e194e595d696301ecd5c84ebf39b85580ea43
Dominik Novi <dominik.novi@htl.rennweg.at>
2019-01-05 12:33:04 +0100

    fixed with this digestCircle thing

--- Frontend/app/components/employee-dropdown.js
+++ Frontend/app/components/employee-dropdown.js
@@ -5,33 +5,16 @@ app.component("employeeDropdown", {
 
 app.controller("employeeDropdownController", function ($http, $rootScope, $scope) {
 
-    this.users = [];
-
-    let url = "../../Backend/SendFirstnameSurnameWithoutLeader.php";
-
-    let parameter = JSON.stringify({
-        projectId: $rootScope.projectId
-    });
-
-    $http({
-        method: 'POST',
-        url: url,
-        data: parameter
-    }).then(
-        (response) => {
-            console.log(response);
-            this.firstnames = response.data.firstnames.split(";");
-            this.surnames = response.data.surnames.split(";");
-            this.userIds = response.data.userIds.split(";");
-        }, function (error) {
-            console.log(error);
-        }).then(() => {
-        for (let i = 0; i < this.surnames.length; i++) {
-            this.users.push((this.firstnames[i] + " " + this.surnames[i]));
+    $rootScope.$watch('projectId', () => {
+        if($rootScope.projectId !== undefined){
+            getEmployees();
         }
     });
 
-    $scope.selectChanged = function () {
+    let getEmployees = () => {
+
+        this.users = [];
+
         let url = "../../Backend/SendFirstnameSurnameWithoutLeader.php";
 
         let parameter = JSON.stringify({
@@ -52,10 +35,36 @@ app.controller("employeeDropdownController", function ($http, $rootScope, $scope
                 console.log(error);
             }).then(() => {
             for (let i = 0; i < this.surnames.length; i++) {
-                if ($scope.selectedUser === (this.firstnames[i] + " " + this.surnames[i])) {
-                    $rootScope.currentEmployee = this.userIds[i];
-                }
+                this.users.push((this.firstnames[i] + " " + this.surnames[i]));
             }
         });
-    };
+
+        $scope.selectChanged = function () {
+            let url = "../../Backend/SendFirstnameSurnameWithoutLeader.php";
+
+            let parameter = JSON.stringify({
+                projectId: $rootScope.projectId
+            });
+
+            $http({
+                method: 'POST',
+                url: url,
+                data: parameter
+            }).then(
+                (response) => {
+                    console.log(response);
+                    this.firstnames = response.data.firstnames.split(";");
+                    this.surnames = response.data.surnames.split(";");
+                    this.userIds = response.data.userIds.split(";");
+                }, function (error) {
+                    console.log(error);
+                }).then(() => {
+                for (let i = 0; i < this.surnames.length; i++) {
+                    if ($scope.selectedUser === (this.firstnames[i] + " " + this.surnames[i])) {
+                        $rootScope.currentEmployee = this.userIds[i];
+                    }
+                }
+            });
+        };
+    }
 });
