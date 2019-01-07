c3bdbd1d52ea4b3951301f63b74f3d0a56f4f18a
KevChela <kevin.chelappurath@htl.rennweg.at>
2019-01-07 14:56:55 +0100

    Fix = Erstellen (Kevin)

--- Frontend/app/components/create-project.js
+++ Frontend/app/components/create-project.js
@@ -12,7 +12,7 @@ app.controller("createProjectController", function ($http, $scope, $mdDialog, $w
             .placeholder('Projektname')
             .targetEvent(ev)
             .required(true)
-            .ok('Bestätigen!')
+            .ok('Erstellen!')
             .cancel('Abbrechen');
 
         $mdDialog.show(confirm).then(function (result) {
